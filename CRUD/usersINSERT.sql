5074f160fdab492aaa31dbb455d78d5092523038
Valeria <melissavlqf@gmail.com>
2026-09-24 17:21:38 -0600

    Inserta dos personas más para observar el comportamiento del auto incremento de la PRIMARY KEY

---
+++ CRUD/usersINSERT.sql
@@ -0,0 +1,6 @@
+-- Usa INSERT INTO para crear tuplas en las tablas
+INSERT INTO users (id, name, age, country_code, username, password, is_admin)
+VALUES (1, "David", 34, "US", "DavidDev", "insertpractice", false);
+
+INSERT INTO users (id, name, age, country_code, username, password, is_admin)
+VALUES (2, "Samantha", 29, "BR", "Sammy93", "addingRecords", false);
