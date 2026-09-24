6e25f458ecafcbf161a6340b1e4acb6b84fde0c3
valeria-quintero <melissavlqf@gmail.com>
2026-09-24 13:25:59 -0600

    Inserta registros en la tabla "users"

--- tables/users.sql
+++ tables/users.sql
@@ -9,5 +9,38 @@ CREATE TABLE users(
   is_admin BOOLEAN
 );
 
+-- Inserta registros en la tabla "users"
+INSERT INTO users(
+  id,
+  name,
+  age,
+  username,
+  password,
+  is_admin
+) values (
+  0,
+  "Valeria",
+  22,
+  "valeriaml",
+  "123",
+  false
+);
+
+INSERT INTO users(
+  id,
+  name,
+  age,
+  username,
+  password,
+  is_admin
+) values (
+  1,
+  "Melissa",
+  22,
+  "melissalq",
+  "456",
+  false
+);
+
 -- Muestra los datos de la tabla
 pragma table_info(' users ');
