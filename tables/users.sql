973f606d6b474eb82ce38cfc26743fe022475e61
valeria-quintero <melissavlqf@gmail.com>
2026-09-24 16:21:45 -0600

    Actualiza la estructura de la tabla "users" y añade la tabla "countries"
    Modified the users table to allow nullable fields for age, username, and password. Added foreign key constraint for country_code and inserted records into the countries table.

--- tables/users.sql
+++ tables/users.sql
@@ -1,45 +1,56 @@
+PRAGMA foreign_keys = ON;
+
+-- Crea una nueva tabla "countries"
+CREATE TABLE countries (code TEXT PRIMARY KEY, name TEXT);
+
 -- Crea la tabla users usando CONSTRAINTS
 CREATE TABLE users(
   id INTEGER PRIMARY KEY,
   name TEXT NOT NULL,
-  age INTEGER NOT NULL,
+  age INTEGER,
   country_code TEXT NOT NULL,
-  username TEXT UNIQUE,
-  password TEXT NOT NULL,
-  is_admin BOOLEAN
+  username TEXT,
+  password TEXT,
+  is_admin BOOLEAN,
+  FOREIGN KEY (country_code) REFERENCES countries(code)
 );
 
 -- Inserta registros en la tabla "users"
 INSERT INTO users(
   id,
   name,
-  age,
-  username,
-  password,
-  is_admin
+  country_code
 ) values (
   0,
   "Valeria",
-  22,
-  "valeriaml",
-  "123",
-  false
+  "US"
 );
 
 INSERT INTO users(
   id,
   name,
-  age,
-  username,
-  password,
-  is_admin
+  country_code
 ) values (
   1,
   "Melissa",
-  22,
-  "melissalq",
-  "456",
-  false
+  "IN"
+);
+
+-- Inserta registros en la tabla countries
+INSERT INTO countries (
+  code,
+  name
+) values (
+  "US",
+  "United States"
+);
+
+INSERT INTO countries (
+  code,
+  name
+) values (
+  "IN",
+  "India"
 );
 
 -- Muestra los datos de la tabla
