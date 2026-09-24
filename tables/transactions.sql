232c5afa43c0f0ceda533c32c3c0605366887671
valeria-quintero <melissavlqf@gmail.com>
2026-09-24 12:55:48 -0600

    Agrega dos columnas a la tabla transactions

--- tables/transactions.sql
+++ tables/transactions.sql
@@ -3,8 +3,16 @@ CREATE TABLE transactions(
   recipient_id INTEGER,
   sender_id INTEGER,
   note TEXT,
-  amount INTEGER
+  amount INTEGER,
+  was_successful BOOLEAN,
+  transaction_type TEXT
 );
+
+-- MIGRATIONS --
+-- Agrega dos columbas a la tabla "transactions"
+ALTER TABLE transactions ADD COLUMN was_successful BOOLEAN;
+ALTER TABLE transactions ADD COLUMN transaction_type TEXT;
+
   
 -- TEST SUITE, DON'T TOUCH BELOW THIS LINE --
 pragma table_info( 'transactions' ) ;
