390e17799c7ba3b134cf3dceca7e42ab918fbeb6
Valeria <melissavlqf@gmail.com>
2026-09-24 19:18:45 -0600

    Emplea cláusulas IN y LIKE

--- basic_queries/tablesLOGICALOPS.sql
+++ basic_queries/tablesLOGICALOPS.sql
@@ -7,3 +7,13 @@ AND age < 18;
 SELECT count(*) FROM users
 WHERE country_code = "US" OR country_code = "CA"
 AND age < 18;
+
+-- IN se utiliza en la cláusula WHERE para comprobar si el valor de una columna coincide con cualquier valor de una lista específica
+ SELECT name, age, country_code
+ FROM users
+ WHERE country_code IN ('US', 'CA', 'MX');
+
+-- LIKE se usa en la cláusula WHERE para buscar un patrón específico de texto dentro de una columna
+SELECT *
+FROM users
+WHERE name LIKE 'Al%';
